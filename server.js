1e85ca5c62c4bf1c76403cc2aa23a5024ce8f05c
jimMehta <jimit1@gmail.com>
2020-07-25 10:34:32 -0700

    modify routes and removed mongodb controllers

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const apiRoutes = require("./routes/test-routes.js");
-const notesRoutes = require("./routes/mongo-routes.js");
+// const apiRoutes = require("./routes/test-routes.js");
+// const notesRoutes = require("./routes/mongo-routes.js");
+const shoeRoutes = require("./routes/shoe-routes");
 
-app.use(apiRoutes, notesRoutes);
+app.use(shoeRoutes);
 
 app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
